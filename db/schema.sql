9d10595591ea6eae97b21dd5cd0bb5a459982920
Michael Cruz <mrcruz1989@gmail.com>
2021-05-05 19:31:12 -0400

    put a pause on solving date problem. moving on to different issues.

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ CREATE DATABASE QA;
 
 USE QA;
 
-CREATE TABLE question (
+CREATE TABLE questions (
   id integer NOT NULL AUTO_INCREMENT,
   product_id integer NOT NULL,
   question varchar(255),
@@ -14,21 +14,21 @@ CREATE TABLE question (
   asker varchar(50),
   email varchar(40),
   reported boolean NOT NULL DEFAULT false,
-  question_helpfulness integer NOT NULL DEFAULT 0,
+  question_helpfulness integer DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE answer (
+CREATE TABLE answers (
   answer_id integer NOT NULL AUTO_INCREMENT,
   question_id integer NOT NULL,
   answer varchar(255),
-  datePosted timestamp,
+  datePosted timestamp DEFAULT CURRENT_TIMESTAMP,
   answerer varchar(50),
   email varchar(40),
   reported boolean NOT NULL DEFAULT false,
-  answer_helpfulness integer NOT NULL DEFAULT 0,
+  answer_helpfulness integer DEFAULT NULL,
   PRIMARY KEY (answer_id),
-  FOREIGN KEY (question_id) REFERENCES question (id)
+  FOREIGN KEY (question_id) REFERENCES questions (id)
 );
 
 CREATE TABLE answerPhotos(
@@ -36,14 +36,36 @@ CREATE TABLE answerPhotos(
   answer_id integer,
   link varchar(255),
   PRIMARY KEY (id),
-  FOREIGN KEY (answer_id) REFERENCES answer (answer_id)
+  FOREIGN KEY (answer_id) REFERENCES answers (answer_id)
 );
 
-LOAD DATA LOCAL INFILE '/Users/mrcruz/HackReactor/SDC-CSV/questionTest.csv'
-INTO TABLE question
+LOAD DATA LOCAL INFILE '/Users/mrcruz/HackReactor/SDC-CSV/questions.csv'
+INTO TABLE questions
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (id, product_id, question, @date, asker, email, reported, question_helpfulness)
   SET datePosted = FROM_UNIXTIME(@date/1000);
+
+-- SHOW WARNINGS;
+
+-- DELETE FROM questions WHERE question_helpfulness IS NULL;
+
+-- LOAD DATA LOCAL INFILE '/Users/mrcruz/HackReactor/SDC-CSV/answerTest.csv'
+-- INTO TABLE answers
+-- FIELDS TERMINATED BY ','
+-- OPTIONALLY ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES
+-- (answer_id, question_id, answer, @date, answerer, email, reported, answer_helpfulness)
+-- SET datePosted = FROM_UNIXTIME(@date/1000);
+
+-- SHOW WARNINGS;
+
+-- LOAD DATA LOCAL INFILE '/Users/mrcruz/HackReactor/SDC-CSV/photoTest.csv'
+-- INTO TABLE answerPhotos
+-- FIELDS TERMINATED BY ','
+-- OPTIONALLY ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES
